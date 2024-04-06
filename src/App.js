4eacb40c8f6a241d515df9077b3453bb3b539308
0codezone <mohittailor997@gmail.com>
2024-04-06 23:00:26 +0530

    create signup form

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
+import CreateProfileForm from "./components/CreateProfileForm";
+import MailVerification from "./components/MailVerification";
+import Role from "./components/Role";
+import SignupForm from "./components/SignupForm";
+
 function App() {
   return (
-    <div className=" flex flex-col justify-top items-center mt-16">
-      <div className="max-w-7xl w-full p-8 text-left">
-        <h1 className="text-5xl font-bold text-stone-700 mb-4">
-          Hello <span className="text-stone-600">World!</span>
-        </h1>
-        <p className="text-base text-gray-700">
-          {" "}
-          Cheers,
-          <br />
-          Start your project 🚀
-        </p>
-      </div>
+    <div className="w-full">
+      <SignupForm />
+      <CreateProfileForm />
+      <Role />
+      <MailVerification />
     </div>
   );
 }
--- src/components/signupForm.js
+++ src/components/signupForm.js
@@ -1,7 +1,111 @@
 import React from "react";
+import logo from "../assets/logo.png";
+import sideImg from "../assets/signupSide.png";
 
-const signupForm = () => {
-  return <div></div>;
+const SignupForm = () => {
+  return (
+    <div className="w-full h-screen flex">
+      <section className=" h-full bg-yellow-300 md:min-w-[400px] hidden md:flex flex-col px-8">
+        <div>
+          <img src={logo} alt="#" width={100} />
+          <h2 className="font-bold text-xl text-orange-800 ">
+            Discover the world's top <br />
+            Designer & Creative
+          </h2>
+        </div>
+
+        <div className="mt-[60px]">
+          <img src={sideImg} alt="#" width={400} />
+          <p className="text-sm mt-[20px]">
+            Art by <span>Mohit Darji</span>
+          </p>
+        </div>
+      </section>
+
+      <section className="h-full w-full p-4">
+        <div className="w-full py-6">
+          <p className="text-sm float-right px-5">
+            Already a member? <span className="text-purple-800">Sign in</span>
+          </p>
+        </div>
+        <div className="w-full flex justify-center items-center py-3 mt-5 px-4 ">
+          <div>
+            <h2 className="text-2xl md:text-3xl font-bold">
+              Sign up to Dribble
+            </h2>
+            <p className="text-red-500 text-xs py-3">
+              Username has been already taken
+            </p>
+            <form>
+              <div className="flex gap-3 py-2 w-full">
+                <div className="flex flex-col w-full">
+                  <label>Name</label>
+                  <input
+                    type="text"
+                    placeholder="Name"
+                    className="outline-none border-none pl-2 p-1 bg-gray-400/30 rounded w-full"
+                  />
+                </div>
+                <div className="flex flex-col w-full">
+                  <label>Username</label>
+                  <input
+                    type="text"
+                    placeholder="Username"
+                    className="outline-none border-none pl-2 p-1 bg-red-400/50 rounded placeholder:text-red-500 w-full"
+                  />
+                </div>
+              </div>
+              <div className="flex flex-col py-2">
+                <label>email</label>
+                <input
+                  type="text"
+                  placeholder="email"
+                  className="outline-none border-none pl-2 p-1 bg-gray-400/30 rounded w-full"
+                />
+              </div>
+              <div className="flex flex-col py-2">
+                <label>password</label>
+                <input
+                  type="text"
+                  placeholder="6+ characters"
+                  className="outline-none border-none pl-2 p-1 bg-gray-400/30 rounded w-full"
+                />
+              </div>
+              <div className="flex gap-2 items-start py-5 text-sm">
+                <input type="checkbox" className="mt-1" />
+                <p className="sm:max-w-[400px] text-xs sm:text-sm">
+                  Creating an account mean's you're okay with out{" "}
+                  <span className="text-purple-800">
+                    Term of Service. Privacy Policy.
+                  </span>{" "}
+                  and our default{" "}
+                  <span className="text-purple-800">
+                    Notification Settigns.
+                  </span>
+                </p>
+              </div>
+              <div className="py-2">
+                <button className="bg-pink-500 text-white px-5 py-1 rounded-md">
+                  Create Account
+                </button>
+              </div>
+            </form>
+            <p className="text-xs py-2 mt-2 text-black/60">
+              This site is protected by reCAPTCHA and the Google <br />
+              <span className="text-purple-800 font-bold">
+                Privacy Policy
+              </span>{" "}
+              and{" "}
+              <span className="text-purple-800 font-bold">
+                Term of Services{" "}
+              </span>
+              apply
+            </p>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
-export default signupForm;
+export default SignupForm;
